dde9daed85fbb2c47a00e040e730a77188fa73b8
David R. Smith <davidsmith@esri.com>
2024-02-28 16:07:40 +0000

    Unused import

--- src/main/java/com/mycompany/app/NatureCameraViewerApp.java
+++ src/main/java/com/mycompany/app/NatureCameraViewerApp.java
@@ -62,7 +62,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
